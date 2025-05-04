7839ebd1c234b29cda4f9ce56065a36d9d5ae864
miwakos <goma.kabo@gmail.com>
2025-05-04 20:55:29 +0900

    Create Human class

---
+++ app/src/main/java/jp/techacademy/miwako/sugishita/kotlinlog/Human.kt
@@ -0,0 +1,14 @@
+package jp.techacademy.miwako.sugishita.kotlinlog
+
+import android.util.Log
+
+open class Human(name: String, age: Int, var hobby: String) : Animal(name, age), Thinkable {
+
+    override fun say() {
+        Log.d(formatTestLogTag(), "私の名前は" + this.name + "です。年は" + this.age + "歳です。")
+    }
+
+    override fun think() {
+        Log.d(formatTestLogTag(), "私は" + this.hobby + "について考える。")
+    }
+}
---
+++ app/src/main/java/jp/techacademy/miwako/sugishita/kotlinlog/Thinkable.kt
@@ -0,0 +1,5 @@
+package jp.techacademy.miwako.sugishita.kotlinlog
+
+interface Thinkable {
+    fun think()
+}
